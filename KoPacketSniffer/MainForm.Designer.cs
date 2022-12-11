0eb821c7095deb5ffeb1d14d9f093d598e54bce4
Çağrı Metin <cgrmetin@gmail.com>
2022-12-11 15:14:56 +0300

    UI Fixed

--- KoPacketSniffer/MainForm.Designer.cs
+++ KoPacketSniffer/MainForm.Designer.cs
@@ -28,41 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gidenGroupBox = new System.Windows.Forms.GroupBox();
-            this.gelenGroupBox = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.gidenListView = new System.Windows.Forms.ListView();
-            this.gelenListView = new System.Windows.Forms.ListView();
             this.startListeningToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.stopListeningToolStripButton = new System.Windows.Forms.ToolStripButton();
-            this.devicesComboBox = new System.Windows.Forms.ComboBox();
             this.refreshToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.devicesComboBox = new System.Windows.Forms.ComboBox();
+            this.gidenGroupBox = new System.Windows.Forms.GroupBox();
+            this.gidenListView = new System.Windows.Forms.ListView();
+            this.gelenGroupBox = new System.Windows.Forms.GroupBox();
+            this.gelenListView = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.gidenGroupBox.SuspendLayout();
             this.gelenGroupBox.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // gidenGroupBox
+            // groupBox1
             // 
-            this.gidenGroupBox.Controls.Add(this.gidenListView);
-            this.gidenGroupBox.Dock = System.Windows.Forms.DockStyle.Left;
-            this.gidenGroupBox.Location = new System.Drawing.Point(0, 50);
-            this.gidenGroupBox.Name = "gidenGroupBox";
-            this.gidenGroupBox.Size = new System.Drawing.Size(503, 630);
-            this.gidenGroupBox.TabIndex = 1;
-            this.gidenGroupBox.TabStop = false;
-            this.gidenGroupBox.Text = "Giden Paketler";
-            // 
-            // gelenGroupBox
-            // 
-            this.gelenGroupBox.Controls.Add(this.gelenListView);
-            this.gelenGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gelenGroupBox.Location = new System.Drawing.Point(503, 50);
-            this.gelenGroupBox.Name = "gelenGroupBox";
-            this.gelenGroupBox.Size = new System.Drawing.Size(609, 630);
-            this.gelenGroupBox.TabIndex = 2;
-            this.gelenGroupBox.TabStop = false;
-            this.gelenGroupBox.Text = "Gelen Paketler";
+            this.groupBox1.Controls.Add(this.toolStrip1);
+            this.groupBox1.Controls.Add(this.devicesComboBox);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1112, 44);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
             // 
             // toolStrip1
             // 
@@ -76,26 +67,6 @@
             this.toolStrip1.Size = new System.Drawing.Size(81, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
-            // 
-            // gidenListView
-            // 
-            this.gidenListView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gidenListView.HideSelection = false;
-            this.gidenListView.Location = new System.Drawing.Point(3, 16);
-            this.gidenListView.Name = "gidenListView";
-            this.gidenListView.Size = new System.Drawing.Size(497, 611);
-            this.gidenListView.TabIndex = 0;
-            this.gidenListView.UseCompatibleStateImageBehavior = false;
-            // 
-            // gelenListView
-            // 
-            this.gelenListView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gelenListView.HideSelection = false;
-            this.gelenListView.Location = new System.Drawing.Point(3, 16);
-            this.gelenListView.Name = "gelenListView";
-            this.gelenListView.Size = new System.Drawing.Size(603, 611);
-            this.gelenListView.TabIndex = 0;
-            this.gelenListView.UseCompatibleStateImageBehavior = false;
             // 
             // startListeningToolStripButton
             // 
@@ -117,16 +88,6 @@
             this.stopListeningToolStripButton.Text = "toolStripButton1";
             this.stopListeningToolStripButton.Click += new System.EventHandler(this.stopListeningToolStripButton_Click);
             // 
-            // devicesComboBox
-            // 
-            this.devicesComboBox.Dock = System.Windows.Forms.DockStyle.Left;
-            this.devicesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.devicesComboBox.FormattingEnabled = true;
-            this.devicesComboBox.Location = new System.Drawing.Point(3, 16);
-            this.devicesComboBox.Name = "devicesComboBox";
-            this.devicesComboBox.Size = new System.Drawing.Size(650, 21);
-            this.devicesComboBox.TabIndex = 1;
-            // 
             // refreshToolStripButton
             // 
             this.refreshToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
@@ -137,6 +98,58 @@
             this.refreshToolStripButton.Text = "toolStripButton1";
             this.refreshToolStripButton.Click += new System.EventHandler(this.refreshToolStripButton_Click);
             // 
+            // devicesComboBox
+            // 
+            this.devicesComboBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.devicesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.devicesComboBox.FormattingEnabled = true;
+            this.devicesComboBox.Location = new System.Drawing.Point(3, 16);
+            this.devicesComboBox.Name = "devicesComboBox";
+            this.devicesComboBox.Size = new System.Drawing.Size(650, 21);
+            this.devicesComboBox.TabIndex = 1;
+            // 
+            // gidenGroupBox
+            // 
+            this.gidenGroupBox.Controls.Add(this.gidenListView);
+            this.gidenGroupBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gidenGroupBox.Location = new System.Drawing.Point(0, 44);
+            this.gidenGroupBox.Name = "gidenGroupBox";
+            this.gidenGroupBox.Size = new System.Drawing.Size(503, 636);
+            this.gidenGroupBox.TabIndex = 1;
+            this.gidenGroupBox.TabStop = false;
+            this.gidenGroupBox.Text = "Giden Paketler";
+            // 
+            // gidenListView
+            // 
+            this.gidenListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gidenListView.HideSelection = false;
+            this.gidenListView.Location = new System.Drawing.Point(3, 16);
+            this.gidenListView.Name = "gidenListView";
+            this.gidenListView.Size = new System.Drawing.Size(497, 617);
+            this.gidenListView.TabIndex = 0;
+            this.gidenListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // gelenGroupBox
+            // 
+            this.gelenGroupBox.Controls.Add(this.gelenListView);
+            this.gelenGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gelenGroupBox.Location = new System.Drawing.Point(503, 44);
+            this.gelenGroupBox.Name = "gelenGroupBox";
+            this.gelenGroupBox.Size = new System.Drawing.Size(609, 636);
+            this.gelenGroupBox.TabIndex = 2;
+            this.gelenGroupBox.TabStop = false;
+            this.gelenGroupBox.Text = "Gelen Paketler";
+            // 
+            // gelenListView
+            // 
+            this.gelenListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gelenListView.HideSelection = false;
+            this.gelenListView.Location = new System.Drawing.Point(3, 16);
+            this.gelenListView.Name = "gelenListView";
+            this.gelenListView.Size = new System.Drawing.Size(603, 617);
+            this.gelenListView.TabIndex = 0;
+            this.gelenListView.UseCompatibleStateImageBehavior = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -144,22 +157,26 @@
             this.ClientSize = new System.Drawing.Size(1112, 680);
             this.Controls.Add(this.gelenGroupBox);
             this.Controls.Add(this.gidenGroupBox);
+            this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
             this.Text = "Ko Sniffer";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
-            this.gidenGroupBox.ResumeLayout(false);
-            this.gelenGroupBox.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.gidenGroupBox.ResumeLayout(false);
+            this.gelenGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton startListeningToolStripButton;
         private System.Windows.Forms.ToolStripButton stopListeningToolStripButton;
